88d543c48c85216d61af102869a79f526bcebd4f
wwenj <man@wwenj.com>
2019-10-29 22:06:27 +0800

    fix

--- server/src/config/router.js
+++ server/src/config/router.js
@@ -1,5 +1,3 @@
 module.exports = [
-  ['/appleText', '/appleAuth/appleText'],
-  ['/appleAuth', '/appleAuth/appleAuth'],
-  ['/.well-known/apple-developer-domain-association.txt', '/appleAuth/known']
+  // ['/appleText', '/appleAuth/appleText'],
 ];
